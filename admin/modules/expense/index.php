3b6974782922f8e01804877fb31bd6e7e8f7c55c
rcstrue <rcstrue@gmail.com>
2026-06-26 17:55:19 +0530

    Delete admin directory

--- admin/modules/expense/index.php
+++
@@ -1,3 +0,0 @@
-<?php $pageTitle = 'Expense Management'; ?>
-<script>window.location.href = 'index.php?page=expense/dashboard';</script>
-<noscript><meta http-equiv="refresh" content="0;url=index.php?page=expense/dashboard"></noscript>
--- admin/modules/loan/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * RCS HRMS Pro - Loan Module Index
- * Redirects to loan list
- */
-header('Location: index.php?page=loan/list');
-exit;
